944a90940338b2157d6b2ea6ddf471cddae11588
Ian Odhiambo <you@example.com>
2019-09-11 10:11:42 +0300

    working blog

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -2,11 +2,17 @@ import React from "react"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import './css/blog.css';
+
 
 const Blog = () => (
   <Layout>
     <SEO title="Blog" />
-    <h1>Blog</h1>
+    <div className="container-fluid blog row">
+				<div className="align-center     mx-auto">
+					<h3 className="display-4 h1">COMING SOON!!</h3>
+				</div>
+			</div>
 
   </Layout>
 )
